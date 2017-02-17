9046795eb607dab8748fda0ff442f7b7021aa8a2
CrystalFaith <crystallambert@Crystals-MacBook-Pro.local>
2017-02-17 15:41:58 -0800

    draft one

--- javascript/game.js
+++ javascript/game.js
@@ -15,54 +15,154 @@ var berry = Math.floor(Math.random() * 19) + 1;
 // console log for clarity
 console.log(cherry, orange, banana, berry);
 
+
+
 // var total is will be the value of var totalGuess added together
-	var total = 0;
-// var totalGUess will hold the value of the fruits once they are clicked on
-    var totalGuess = [];
+var total = 0;
 
-// when cherry is clicked
-$("#cherry").on("click", function(){
-	// we push the value of cherry to the arry
-	totalGuess.push(cherry);
-// this is supposed to update var totoal to the array added together
-	for(i = 0; i < totalGuess.length; i++){
-	total += totalGuess[i];};
+win = 0;
+loss = 0;
 
-	// show what our totoal is and what is in the array
-	console.log(total);
-	console.log(totalGuess);
+
+function playGame(){
+
+
+
+	$("#playerScore").html(total);
+	$("#wins").html("Wins: " + win);
+	$("#loss").html("Losses: " + win);
+
+	// when cherry is clicked
+	$("#cherry").on("click", function(){
+
+	// adds totoal with value of fruit
+		total = total + cherry;
+		$("#playerScore").html(total);
+		// show what our totoal is
+		console.log(total);
+
+		if(gameWin < total) {
+			loseReset();
+
+		}if(gameWin === total){
+			winReset();
+
+		};
+		
+	});
+
+	$("#orange").on("click", function(){
+
+	// adds totoal with value of fruit
+		total = total + orange;
+		$("#playerScore").html(total);
+		// show what our totoal is
+		console.log(total);
+
+		if(gameWin < total) {
+			loseReset();
+
+		}if(gameWin === total){
+			winReset();
+
+		};
+
+	});
+
+	$("#banana").on("click", function(){
+
+		// adds totoal with value of fruit
+		total = total + banana;
+		$("#playerScore").html(total);
+		// show what our totoal is
+		console.log(total);
+
+		if(gameWin < total) {
+			loseReset();
+
+		}if(gameWin === total){
+			winReset();
+
+		};
+
+	});
+
+	$("#berry").on("click", function(){
+		// adds totoal with value of fruit
+		total = total + berry;
+	$("#playerScore").html(total);
+		// show what our totoal is
+		console.log(total);
+
+		if(gameWin < total) {
+			loseReset();
+
+		}if(gameWin === total){
+			winReset();
+
+		};
+
+	});
+
+
+
+
+
+};
+
+
+
+
+playGame();
+
+
+
+function winReset(){
+
+	gameWin = Math.floor(Math.random() * 120) + 19;
+
+	// display gameWin in #gameNumber div
+	$("#gameNumber").html(gameWin);
+
+	// create random numbers for fruit
+
+	cherry = Math.floor(Math.random() * 19) + 1;
+	orange = Math.floor(Math.random() * 19) + 1;
+	banana = Math.floor(Math.random() * 19) + 1;
+	berry = Math.floor(Math.random() * 19) + 1;
+
+	win++;
+	alert("you win");
+	total = 0;
+	$("#playerScore").html(total);
+	$("#wins").html("Wins: " + win);
+	console.log(cherry, orange, banana, berry);
+
+
+};
+
+function loseReset(){
+	gameWin = Math.floor(Math.random() * 120) + 19;
+
+	// display gameWin in #gameNumber div
+	$("#gameNumber").html(gameWin);
+
+	// create random numbers for fruit
+
+	cherry = Math.floor(Math.random() * 19) + 1;
+	orange = Math.floor(Math.random() * 19) + 1;
+	banana = Math.floor(Math.random() * 19) + 1;
+	berry = Math.floor(Math.random() * 19) + 1;
+
 	
-});
+	alert("you lose");
+	loss++;
+	total = 0;
+	$("#playerScore").html(total);
+	$("#loss").html("Losses: " + loss);
+	console.log(cherry, orange, banana, berry);
 
-$("#orange").on("click", function(){
-	totalGuess.push(orange);
-
-		for(i = 0; i < totalGuess.length; i++){
-	total += totalGuess[i];};
-	
-	console.log(total);
-});
-
-$("#banana").on("click", function(){
-	totalGuess.push(banana);
-		for(i = 0; i < totalGuess.length; i++){
-	total += totalGuess[i];};
-	
-	console.log(total);
-});
-
-$("#berry").on("click", function(){
-	
-	totalGuess.push(berry);
-
-		for(i = 0; i < totalGuess.length; i++){
-	total += totalGuess[i];};
-	
-	console.log(total);
-});
-
-
-
+}
 
 
 // give random num values to fruit
