176583fa34cb0bacba224c02e4d35c982a141247
dblanchard13 <davidb.prof@gmail.com>
2017-03-07 20:51:48 -0800

    Homework Comments.

--- javascript/game.js
+++ javascript/game.js
@@ -1,117 +1,69 @@
+$(document).ready(function () {
+	var orange = $('#orange');
+	var cherry = $('#cherry');
+	var banana = $('#banana');
+	var berry = $('#berry');
 
-// random number to try and add too
+	var gameWin;
+	var total;
 
-var gameWin = Math.floor(Math.random() * 120) + 19;
+	var win = 0;
+	var loss = 0;
 
-// display gameWin in #gameNumber div
-$("#gameNumber").html(gameWin);
+	function resetGameNumbers () {
+		gameWin = generateRandomNumber(19, 120);
+		$("#gameNumber").html(gameWin);
 
-// create random numbers for fruit
+		cherry.data('fruit-val', generateRandomNumber(1, 19)); 
+		orange.data('fruit-val', generateRandomNumber(1, 19)); 
+		banana.data('fruit-val', generateRandomNumber(1, 19)); 
+		berry.data('fruit-val', generateRandomNumber(1, 19)); 
 
-var cherry = Math.floor(Math.random() * 19) + 1;
-var orange = Math.floor(Math.random() * 19) + 1;
-var banana = Math.floor(Math.random() * 19) + 1;
-var berry = Math.floor(Math.random() * 19) + 1;
-
-// console log for clarity
-console.log(cherry, orange, banana, berry);
-
-
-
-// var total is will be the value of var totalGuess added together
-var total = 0;
-
-win = 0;
-loss = 0;
-
-function winReset(){
-
-	gameWin = Math.floor(Math.random() * 120) + 19;
-
-	// display gameWin in #gameNumber div
-	$("#gameNumber").html(gameWin);
-
-	// create random numbers for fruit
-
-	var cherry = Math.floor(Math.random() * 19) + 1;
-	var orange = Math.floor(Math.random() * 19) + 1;
-	var banana = Math.floor(Math.random() * 19) + 1;
-	var berry = Math.floor(Math.random() * 19) + 1;
-
-	win++;
-	alert("you win");
-	total = 0;
-	$("#playerScore").html(total);
-	$("#wins").html("Wins: " + win);
-	console.log(cherry, orange, banana, berry);
-
-
-};
-
-function loseReset(){
-	gameWin = Math.floor(Math.random() * 120) + 19;
-
-	// display gameWin in #gameNumber div
-	$("#gameNumber").html(gameWin);
-
-	// create random numbers for fruit
-
-	var cherry = Math.floor(Math.random() * 19) + 1;
-	var orange = Math.floor(Math.random() * 19) + 1;
-	var banana = Math.floor(Math.random() * 19) + 1;
-	var berry = Math.floor(Math.random() * 19) + 1;
-
-	
-	alert("you lose");
-	loss++;
-	total = 0;
-	$("#playerScore").html(total);
-	$("#loss").html("Losses: " + loss);
-	console.log(cherry, orange, banana, berry);
-
-}
-
-function playGame(){
-
-	$("#playerScore").html(total);
-	$("#wins").html("Wins: " + win);
-	$("#loss").html("Losses: " + win);
-
-	// when cherry is clicked
-	$(".fruitimg").on("click", function(event){
-
-		if(event.currentTarget.attributes.id.nodeValue === "cherry"){
-	// adds totoal with value of fruit
-		total = total + cherry;
+		total = 0;
 		$("#playerScore").html(total);
-		// show what our totoal is
-		console.log(total);
-		console.log();
-		}
+	};
 
-		if(event.currentTarget.attributes.id.nodeValue === "orange"){
-			total = total + orange;
+	function generateRandomNumber (min, max) {
+		max -= min
+		return Math.floor(Math.random() * max) + min;
+	};
+
+	function winReset () {
+		win ++;
+		alert("you win");
+		$("#wins").html("Wins: " + win);
+		resetGameNumbers();
+	};
+
+	function loseReset () {
+		loss ++;
+		alert("you lose");
+		$("#loss").html("Losses: " + loss);
+		resetGameNumbers();
+	};
+
+	function playGame () {
+
+		$("#playerScore").html(total);
+		$("#wins").html("Wins: " + win);
+		$("#loss").html("Losses: " + win);
+
+		$(".fruitimg").on("click", function(event){
+
+			total += $(this).data('fruit-val')
 			$("#playerScore").html(total);
-		}
-		if(event.currentTarget.attributes.id.nodeValue === "banana"){
-			total = total + banana;
-			$("#playerScore").html(total);
-		}
-		if(event.currentTarget.attributes.id.nodeValue === "berry"){
-			total = total + berry;
-			$("#playerScore").html(total);
-		}
 
-		if(gameWin < total) {
-			loseReset();
+			if (gameWin < total) {
+				loseReset();
+			} else if (gameWin === total) {
+				winReset();
+			};
+			
+		});
 
-		}if(gameWin === total){
-			winReset();
+	};
 
-		};
-		
-	});
-};
-playGame();
+	resetGameNumbers();
+	playGame();
 
-
+})
